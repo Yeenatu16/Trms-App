d0e88d642bc6a42f73504860a655dbc3f2b2febd
surafelberhe <suraberhe@gmail.com>
2026-08-28 10:05:11 +0300

    chore: update frontend configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // @ts-ignore
+  allowedDevOrigins: ['192.168.1.8', '192.168.1.4', '192.168.1.7', '127.0.0.1', 'localhost'],
 };
 
 export default nextConfig;
+
