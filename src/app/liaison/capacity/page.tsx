5174937ced5084cc48f732953ad8d44698573965
surafelberhe <suraberhe@gmail.com>
2026-08-28 10:23:02 +0300

    feat: add liaison officer interface

---
+++ src/app/liaison/capacity/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from 'react';
+import FacilityMasterDetail from '@/components/facilities/FacilityMasterDetail';
+
+export default function LiaisonCapacityPage() {
+  return <FacilityMasterDetail role="LIAISON_OFFICER" />;
+}
